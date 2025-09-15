561e7eab8c2c3d7ba632d6506ca417938863fa1c
mdadeel <mdadeel125@gmail.com>
2025-09-16 03:18:59 +0600

    ...

---
+++ random rgb color/script.js
@@ -0,0 +1,32 @@
+const generateRgbColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r}, ${g}, ${b})`;
+};
+
+function updateColor() {
+    const color = generateRgbColor();
+    const colorBox = document.getElementById("colorBox");
+    colorBox.style.backgroundColor = color;
+
+    const colorCode = document.getElementById("colorCode");
+    colorCode.innerText = color;
+}
+
+const copyColorCode = () => {
+    const colorCode = document.getElementById("colorCode");
+    const inputtag = document.createElement('input');
+    document.body.appendChild(inputtag);
+
+    inputtag.value = colorCode.innerText;
+    inputtag.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(inputtag);
+    alert("Color code copied!");
+};
+
+document.getElementById("copyBtn").addEventListener("click", copyColorCode);
+
+updateColor();
